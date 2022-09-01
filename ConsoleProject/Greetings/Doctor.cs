c6d89b4d4cf222196e3cd162f4fd10832c954545
Brandon Eiler <eiler.brandon@gmail.com>
2022-08-31 17:50:05 -0700

    added greetings exercise to project

---
+++ ConsoleProject/Greetings/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greetings
+{
+    internal class Doctor : Person
+    {
+        float Salary = 0.0f;
+
+        public Doctor()
+        {
+            this.name = "John Doe";
+        }
+
+        public Doctor (string name)
+        {
+            this.name = name;
+        }
+
+        override public void SayGreeting()
+        {
+            Console.WriteLine("Hello, I'm Dr. " + name);
+
+        }
+    }
+}
---
+++ ConsoleProject/Greetings/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Person
+    {
+        protected string name = "";
+        protected int phoneNumber = 0;
+        protected string emailAddress = "";
+
+        //default constructor
+        public Person()
+        {
+            this.name = "John Doe";
+        }
+
+        //one argument constructor (name)
+        public Person (string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void SayGreeting()
+        {
+            Console.WriteLine("Hello, I'm " + name);
+        }
+    }
+
+}
